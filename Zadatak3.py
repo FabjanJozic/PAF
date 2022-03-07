c4564481a92f2babb7831f9829dd2256e4447154
FabjanJozic <fjozic@pmfst.hr>
2022-03-07 20:37:03 +0100

    Prepravljanje koda i dodavanje mogucnosti pogreske.

--- Zadatak3.py
+++ Zadatak3.py
@@ -1,15 +1,23 @@
-A1 = int(input('Upiši vrijednost x koordinate prve točke: '))
-A2 = int(input('Upiši vrijednost y koordinate prve točke: '))
-B1 = int(input('Upiši vrijednost x koordinate druge točke: '))
-B2 = int(input('Upiši vrijednost y koordinate prve točke: '))
 
-k = (B2 - A2)/(B1 - A1)
-a = -k*A1 + A2
+try:
+    A1 = int(input('Upiši vrijednost x koordinate prve točke: '))
+    A2 = int(input('Upiši vrijednost y koordinate prve točke: '))
+    B1 = int(input('Upiši vrijednost x koordinate druge točke: '))
+    B2 = int(input('Upiši vrijednost y koordinate prve točke: '))
 
-if a <0:
-    print('Jednadžba pravca kroz te dvije točke je  {}'.format('y='+str(k)+'x'+str(a)))
-elif a >0:
-    print('Jednadžba pravca kroz te dvije točke je  {}'.format('y='+str(k)+'x+'+str(a)))
-else:
-    print('Jednadžba pravca kroz te dvije točke je  {}'.format('y='+str(k)+'x'))
+    k = (B2 - A2)/(B1 - A1)
+    a = -k*A1 + A2
+
+    if a <0:
+        print('Jednadžba pravca kroz te dvije točke je  {}'.format('y='+str(k)+'x'+str(a)))
+    elif a >0:
+        print('Jednadžba pravca kroz te dvije točke je  {}'.format('y='+str(k)+'x+'+str(a)))
+    else:
+        print('Jednadžba pravca kroz te dvije točke je  {}'.format('y='+str(k)+'x'))
+
+except ValueError:
+    print('\n'+'Pogreška pri upisu. Vrijednost može biti samo {}.'.format(type(1))+'\n')
+except NameError:
+    print('\n'+'Pogreška pri upisu. Vrijednost može biti samo {}.'.format(type(1))+'\n')
+
 
