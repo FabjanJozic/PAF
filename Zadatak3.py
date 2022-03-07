b5566a25ba19d50502c7edf5e3792eb81dccef8d
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2022-03-07 20:16:21 +0100

    Nadogradnja koda zbog mogucnosti pogreske.

--- Zadatak3.py
+++ Zadatak3.py
@@ -1,7 +1,11 @@
-A1 = int(input('Upiši vrijednost x koordinate prve točke: '))
-A2 = int(input('Upiši vrijednost y koordinate prve točke: '))
-B1 = int(input('Upiši vrijednost x koordinate druge točke: '))
-B2 = int(input('Upiši vrijednost y koordinate prve točke: '))
+
+try:
+    A1 = int(input('Upiši vrijednost x koordinate prve točke: '))
+    A2 = int(input('Upiši vrijednost y koordinate prve točke: '))
+    B1 = int(input('Upiši vrijednost x koordinate druge točke: '))
+    B2 = int(input('Upiši vrijednost y koordinate prve točke: '))
+except ValueError:
+    print('Pogreška pri upisu. Vrijednost mora biti {}.'.format(type(1)))
 
 k = (B2 - A2)/(B1 - A1)
 a = -k*A1 + A2
