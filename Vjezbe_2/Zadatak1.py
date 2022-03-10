1c7f4ef686eb6b5d404000f0fe441acad422dd8e
FabjanJozic <100929336+FabjanJozic@users.noreply.github.com>
2022-03-10 20:58:12 +0100

    Preinaka zadatka 1.

--- Vjezbe_2/Zadatak1.py
+++ Vjezbe_2/Zadatak1.py
@@ -1,11 +1,42 @@
-import matplotlib as plo
-import numpy as np 
+import numpy as np
+import matplotlib as plt
 
 
-def jednostavno_gibanje(F, m):
+def gibanje():
     F = float(input('Upiši vrijednost sile u N koja djeluje na tijelo: '))
-    m = float(input('Upiši masu tijela u kg na koje djeluje sila: '))
-    dt = 0.0001
-    t = [i+dt while i < 10]
+    m = float(input('Upiši masu tijela u kg na kojeg djeluje sila: '))
 
-    return print(t)
+    a = F/m
+    t = np.linspace(0, 10, 10000)
+    x = []
+    v = []
+    x0 = 0
+    v0 = 0
+    for i in t:
+        v0 += a*t[i]
+        v.append(v0)
+    for e in v:
+        x0 += t*v[e]
+        x.append(x0)
+
+    fig, axes = plt.subplots(1, 3, figsize=(8, 6), dpi=250)
+    axes.set_aspect('equal')
+    plt.axis('equal')
+    axes[0].plot(a, t, 'b')
+    axes[0].set_xlabel('$t [s]$')
+    axes[0].set_ylabel('$a [m/s^2]$')
+    axes[0].set_title('a-t dijagram')
+    axes[0].grid()
+    axes[1].plot(v, t, 'r')
+    axes[1].set_xlabel('$t [s]$')
+    axes[1].set_ylabel('$v [m/s]$')
+    axes[1].set_title('v-t dijagram')
+    axes[1].grid()
+    axes[2].plot(x, t, 'g')
+    axes[2].set_xlabel('$t [s]$')
+    axes[2].set_ylabel('$x [m]$')
+    axes[2].set_title('x-t dijagram')
+    axes[2].grid()
+    fig.tight_layout()
+
+    return plt.show()
