4468087bc320132681cc871b23f802c2eb25493b
FabjanJozic <fjozic@pmfst.hr>
2022-03-10 09:56:11 +0100

    .

---
+++ Vjezbe_2/Zadatak1.py
@@ -0,0 +1,11 @@
+import matplotlib as plo
+import numpy as np 
+
+
+def jednostavno_gibanje(F, m):
+    F = float(input('Upiši vrijednost sile u N koja djeluje na tijelo: '))
+    m = float(input('Upiši masu tijela u kg na koje djeluje sila: '))
+    dt = 0.0001
+    t = [i+dt while i < 10]
+
+    return print(t)
