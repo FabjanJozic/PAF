7662411ce97d9598a23a823019d447b46119078f
FabjanJozic <fjozic@pmfst.hr>
2022-03-18 12:16:11 +0100

    Prepravljanje zadatka 1.

--- Vjezbe_3/Zadatak1.py
+++ Vjezbe_3/Zadatak1.py
@@ -7,87 +7,72 @@ import os
 
 
 class Particle:
-    def __init__(self, v0, kut, x0, y0):
-        self.v0 = v0
-        self.kut = kut
-        self.x0 = x0
-        self.y0 = y0
+    def __init__(self):
+        self.vx = []
+        self.vy = []
+        self.t = []
+        self.kut = []
+        self.x0 = []
+        self.y0 = []
+        self.dt = 0.001
+        self.ax = []
+        self.ay = []
         
     def set_initial_conditions(self, v0, kut, x0, y0):
         '''
         Funkcija postavlja nove vrijednosti klase.
         '''
-        self.v0 = v0
-        self.kut = kut
-        self.x0 = x0
-        self.y0 = y0
+        self.vx.append(v0 * np.cos((kut / 180)*np.pi))
+        self.vy.append(v0 * np.sin((kut / 180)*np.pi))
+        self.t.append(0)
+        self.kut.append(kut)
+        self.x0.append(x0)
+        self.y0.append(y0)
+        self.dt = 0.001
+        self.ax = [0]
+        self.ay.append(-9.81)
     
     def reset(self):
         '''
         Funkcija resetira i poništava vrijednosti klase.
         '''
-        self.v0 = None 
-        self.kut = None 
-        self.x0 = None
-        self.y0 = None 
+        self.__init__()
     
-    def move(self, dt):
+    def __move(self):
         '''
-        Funkcija pomiče objekt u vremenskom intervalu dt koji zadaje korisnik.
-        Objekt ima putanju kosog hica.
+        Funkcija jednom pomiče objekt u vremenskom intervalu dt.
         '''
-        kut_rad = (self.kut / 180)*np.pi     #type: ignore
-        xbrzina = [self.v0 *np.cos(kut_rad)]
-        ybrzina = [self.v0 *np.sin(kut_rad)]
-        xpomak = [self.x0]
-        ypomak = [self.y0]
-        g = 9.81
-        xbrzina.append(self.v0 *np.cos(kut_rad))
-        ybrzina.append(ybrzina[0]-g*(dt))
-        xpomak.append(xpomak[0] + xbrzina[-1]*(dt))
-        ypomak.append(ypomak[0] + ybrzina[-1]*(dt)) 
-        dx = xpomak[-1]
-        dy = ypomak[-1]
-        return dx, dy
+        self.t.append(self.t[-1]+ self.dt)
+        self.vx.append(self.vx[-1] + self.ax[-1]*self.dt)
+        self.vy.append(self.vy[-1]+ self.ay[-1]*self.dt)
+        self.x0.append(self.x0[-1]+ self.vx[-1]*self.dt)
+        self.y0.append(self.y0[-1]+ self.vy[-1]*self.dt)
+        return self.x0[-1], self.y0[-1]
     
-    def range(self, t):
+    def range(self):
         '''
-        Funkcija računa domet objekta ako ima putanju kosog hica. Korisnik zadaje vrijeme
-        tijek kojega se odvija gibanje.
+        Funkcija računa domet objekta ako ima putanju kosog hica.
         '''
-        kut_rad = (self.kut / 180)*np.pi        #type: ignore
-        xbrzina = [self.v0 *np.cos(kut_rad)]
-        dt = 0.001
-        xpomak = [self.x0]
-        for i in range(int(1000*t)):
-            xbrzina.append(self.v0*np.cos(kut_rad))
-            xpomak.append(xpomak[i]+xbrzina[i]*(dt))   
-        xpomak.sort(reverse=True)
-        domet = xpomak[0]
-        return domet
+        while self.y0[-1] >= 0:
+            self.__move()
+        return self.x0[-1]
     
-    def plot_trajectory(self, t):
+    def plot_trajectory(self):
         '''
         Funkcija crta xy-graf gibanja objekta opisanog kao kosi hitac u vremenu koje zadaje
         korisnik.
         '''
-        kut_rad = (self.kut / 180)*np.pi        #type: ignore
-        xbrzina = [self.v0 *np.cos(kut_rad)]
-        ybrzina = [self.v0 *np.sin(kut_rad)]
-        g = 9.81
-        dt = 0.001
-        xpomak = [self.x0]
-        ypomak = [self.y0]
-        for i in range(int(1000*t)):
-            xbrzina.append(self.v0 *np.cos(kut_rad))
-            ybrzina.append(ybrzina[i]-g*(dt))
-            xpomak.append(xpomak[i]+xbrzina[i]*(dt))
-            ypomak.append(ypomak[i]+ybrzina[i]*(dt))   
+        while self.y0[-1] >= 0:
+            self.__move()  
         fig = plt.figure(figsize=(5,3), dpi=200)
         axes = fig.add_axes([0.15, 0.15, 0.75, 0.75])
-        axes.plot(xpomak, ypomak, 'r')
+        axes.plot(self.x0, self.y0, 'r')
         axes.set_xlabel('$x | [m]$')
         axes.set_ylabel('$y | [m]$')
         axes.grid()
         return plt.show()
     
+p1 = Particle()
+p1.set_initial_conditions(30, 20, 0, 0)
+p1.plot_trajectory()
+print(p1.range())
---
+++ Vjezbe_3/Zadatak2.py
@@ -0,0 +1,2 @@
+import Zadatak1 as prt
+
