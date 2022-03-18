3e037566594ef378b44f2614aef2daa656aa2efa
FabjanJozic <fjozic@pmfst.hr>
2022-03-18 20:19:23 +0100

    Predavanje zadatka 1 i 2.

--- Vjezbe_3/Zadatak1.py
+++ Vjezbe_3/Zadatak1.py
@@ -14,7 +14,7 @@ class Particle:
         self.kut = []
         self.x0 = []
         self.y0 = []
-        self.dt = 0.001
+        self.dt = []
         self.ax = []
         self.ay = []
         
@@ -28,8 +28,8 @@ class Particle:
         self.kut.append(kut)
         self.x0.append(x0)
         self.y0.append(y0)
-        self.dt = 0.001
-        self.ax = [0]
+        self.dt.append(0.001)
+        self.ax.append(0)
         self.ay.append(-9.81)
     
     def reset(self):
@@ -38,23 +38,23 @@ class Particle:
         '''
         self.__init__()
     
-    def __move(self):
+    def __move(self, dt):
         '''
         Funkcija jednom pomiče objekt u vremenskom intervalu dt.
         '''
-        self.t.append(self.t[-1]+ self.dt)
-        self.vx.append(self.vx[-1] + self.ax[-1]*self.dt)
-        self.vy.append(self.vy[-1]+ self.ay[-1]*self.dt)
-        self.x0.append(self.x0[-1]+ self.vx[-1]*self.dt)
-        self.y0.append(self.y0[-1]+ self.vy[-1]*self.dt)
+        self.t.append(self.t[-1]+ dt)
+        self.vx.append(self.vx[-1] + self.ax[-1]*dt)
+        self.vy.append(self.vy[-1]+ self.ay[-1]*dt)
+        self.x0.append(self.x0[-1]+ self.vx[-1]*dt)
+        self.y0.append(self.y0[-1]+ self.vy[-1]*dt)
         return self.x0[-1], self.y0[-1]
     
-    def range(self):
+    def range(self, dt):
         '''
         Funkcija računa domet objekta ako ima putanju kosog hica.
         '''
         while self.y0[-1] >= 0:
-            self.__move()
+            self.__move(dt)
         return self.x0[-1]
     
     def plot_trajectory(self):
@@ -63,16 +63,12 @@ class Particle:
         korisnik.
         '''
         while self.y0[-1] >= 0:
-            self.__move()  
+            self.__move(0.001)  
         fig = plt.figure(figsize=(5,3), dpi=200)
         axes = fig.add_axes([0.15, 0.15, 0.75, 0.75])
-        axes.plot(self.x0, self.y0, 'r')
+        axes.plot(self.x0, self.y0, 'g')
         axes.set_xlabel('$x | [m]$')
         axes.set_ylabel('$y | [m]$')
         axes.grid()
         return plt.show()
     
-p1 = Particle()
-p1.set_initial_conditions(30, 20, 0, 0)
-p1.plot_trajectory()
-print(p1.range())
--- Vjezbe_3/Zadatak2.py
+++ Vjezbe_3/Zadatak2.py
@@ -1,2 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
 import Zadatak1 as prt
+
+v0 = 10
+kut = 60
+g = 9.81
+vrijeme = np.arange(0.001, 1, 0.001)
+error = []
+
+tijelo = prt.Particle()             #type: ignore
+tijelo.set_initial_conditions(v0, kut, 0, 0)
+
+i = 0.001
+while i <= 1:
+    error.append((abs(((v0)**2 /g)*np.sin(2*(kut / 180)*np.pi) - tijelo.range(i)) / ((v0)**2 /g)*np.sin(2*(kut / 180)*np.pi))*100)
+    tijelo.reset()
+    tijelo.set_initial_conditions(v0, kut, 0, 0)
+    i += 0.001
+
+fig = plt.figure(figsize=(12,4.5), dpi=120)
+axes = fig.add_axes([0.1, 0.1, 0.85, 0.75])
+axes.plot(vrijeme, error, 'k', lw=0.6)
+axes.set_xlabel('vrijednost dt    [s]')
+axes.set_ylabel('odstupanje od analitičkog riješenja    [%]')
+axes.xaxis.set_major_locator(ticker.MultipleLocator(0.05))
+axes.set_title('Graf pogreške')
+axes.grid(lw=0.25)
+plt.show()
 
