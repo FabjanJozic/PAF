cd518cefa989e625fef8ef6b246743a08e1e0333
FabjanJozic <fjozic@pmfst.hr>
2022-05-05 23:08:54 +0200

    Popravljanje zadataka da napokon prorade. Problem bio u konstruktoru klase.

--- Vjezbe_7/Particle.cpp
+++ Vjezbe_7/Particle.cpp
@@ -4,35 +4,6 @@
 
 using namespace std;
 
-void Particle::move(){
-
-    vx += 0.;
-    vy += g*dt;
-    x += vx*dt;
-    y += vy+dt;
-    t += dt;
-
-}
-
-double Particle::range(){
-    while(y >= 0){
-    move();
-    }
-    cout << x <<endl;
-    return x;
-}
-
-double Particle::time(){
-    while(y >= 0){
-    move();
-    }
-    cout << t <<endl;
-    return t;
-}
-
-
-
-
 
 Particle::Particle(double v0, double alpha, double x0, double y0, double step){
 
@@ -41,6 +12,33 @@ Particle::Particle(double v0, double alpha, double x0, double y0, double step){
     vy = v0*sin(alpha);
     x = x0;
     y = y0;
+    dt = step;
 
 }
+
+
+void Particle::move(){
+    while(y >= 0){
+    vx += 0.;
+    vy += g*dt;
+    x += vx*dt;
+    y += vy+dt;
+    t += dt;
+    }
+}
+
+double Particle::range(){
+    move();
+    cout << x <<endl;
+    return x;
+}
+
+double Particle::time(){
+    move();
+    cout << t <<endl;
+    return t;
+}
+
+
+
 
--- Vjezbe_7/Particle.h
+++ Vjezbe_7/Particle.h
@@ -8,15 +8,6 @@ class Particle {
     double dt;
     double g = -9.81;
 
-    /*
-    Particle(double v0, double alpha, double x0, double y0, double step = 0.001){
-        vx = v0*cos(alpha);
-        vy = v0*sin(alpha);
-        x = x0;
-        y = y0;
-        dt = step;
-    } */
-
     void move();
 
 
