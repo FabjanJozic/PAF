44a1effb1cb6ac825435b161b4999e6549a4d6fb
FabjanJozic <fjozic@pmfst.hr>
2022-05-30 12:11:15 +0200

    Predavanje domaceg 5.

--- Domaci_5/Zadatak1.py
+++ Domaci_5/Zadatak1.py
@@ -5,34 +5,36 @@ import numpy as np
 
 
 E = np.array([0., 0., 0.])
+B = np.array([0., 0., em.power(4.25, -10)])
 
-def function_B0(t):
-    Bx = 0.
-    By = 0.
-    Bz = em.power(4.25, -10)
+def B_f(t):
+    Bx = 0.0
+    By = 0.0
+    Bz = t/10
     return np.array([Bx, By, Bz])
 
-def function_B(t):
-    Bx = 0.
-    By = 0.
-    Bz = t/10
+def B_f0(t):
+    Bx = 0.0
+    By = 0.0
+    Bz = em.power(4.25, -10)
     return np.array([Bx, By, Bz])
 
 coordinates = np.array([0., 0., 0.])
 mass = em.power(9.11, -31)
 charge_e = em.power(-1, -19)
 charge_p = em.power(1, -19)
-dt = 0.001
+dt = 0.0005
 time = 10
 velocity = np.array([150., 150., 10.])
 
-ele1 = em.ElectromagneticField(coordinates, mass, charge_e, E, function_B, velocity)
-ele2 = em.ElectromagneticField(coordinates, mass, charge_e, E, function_B0, velocity)
-poz = em.ElectromagneticField(coordinates, mass, charge_p, E, function_B, velocity)
+ele1 = em.ElectromagneticField(coordinates, mass, charge_e, E, B, velocity, B_f)
+ele2 = em.ElectromagneticField(coordinates, mass, charge_e, E, B, velocity, B_f0)
+poz = em.ElectromagneticField(coordinates, mass, charge_p, E, B, velocity, B_f)
 
-x1, y1, z1 = ele1.moveit_RK4(dt, time)
-x2, y2, z2 = ele2.moveit_RK4(dt, time)
-x3, y3, z3 = poz.moveit_RK4(dt, time)
+x1, y1, z1 = ele1.moveit_f(dt, time)
+x2, y2, z2 = ele2.moveit_f(dt, time)
+x3, y3, z3 = poz.moveit_f(dt, time)
+
 
 
 fig = plt.figure(figsize=(16, 9), dpi=70)
--- Domaci_5/electromagnetic_field.py
+++ Domaci_5/electromagnetic_field.py
@@ -16,88 +16,72 @@ def power(num, exp):
         result = 1
     elif exp < 0:
         while i < abs(exp):
-            result *= 0.1
+            result /= 10
             i += 1
     return result
 
 
 class ElectromagneticField():
-    def __init__(self, coordinates, mass, charge, E, B, v):
-        self.r = coordinates
-        self.x = [self.r[0]]
-        self.y = [self.r[1]]
-        self.z = [self.r[2]]
-        self.v = v
-        self.t = 0
+    def __init__(self, coordinates, mass, charge, E, B, velocity, func):
+        self.x = []
+        self.y = []
+        self.z = []
         self.m = mass
         self.q = charge
+        self.r = coordinates
+        self.x.append(self.r[0])
+        self.y.append(self.r[1])
+        self.z.append(self.r[2])
+        self.v = velocity
         self.E = E
-        self.function = B
-        self.B = B(self.t)
-        self.a = self.__acceleration(self.v, self.B)
-       
-    def __acceleration(self, v, B):
-        return (self.q/self.m)*np.add(self.E, np.cross(v, B))
-    
-    def __move_Euler(self, dt):
-        self.B = self.function(self.t)
-        self.a = self.__acceleration(self.v, self.B)
-        self.v = np.add(self.v, self.a*dt)
-        self.r = np.add(self.r, self.v*dt)
-        self.t += dt
-        self.x.append(self.r[0])
-        self.y.append(self.r[1])
-        self.z.append(self.r[2])
-                
-    def __move_RK4(self, dt):
-        self.B = self.function(self.t)
-        k1v = self.__acceleration(self.v, self.B)*dt 
-        k1r = self.v*dt
-        k2v = self.__acceleration(np.add(self.v, k1v/2), self.B)*dt
-        k2r = (np.add(self.v,k1v/2))*dt
-        k3v = self.__acceleration(np.add(self.v, k2v/2), self.B)*dt
-        k3r = (np.add(self.v,k2v/2))*dt
-        k4v = self.__acceleration(np.add(self.v, k3v), self.B)*dt
-        k4r = (np.add(self.v,k3v))*dt
-        self.v = np.add(self.v,(1/6)*np.add(np.add(k1v,2*k2v),np.add(2*k3v,k4v)))
-        self.r = np.add(self.r,(1/6)*np.add(np.add(k1r,2*k2r),np.add(2*k3r,k4r)))
-        self.a = self.__acceleration(self.v, self.B)
-        self.x.append(self.r[0])
-        self.y.append(self.r[1])
-        self.z.append(self.r[2])
-        self.t += dt
-    
-    def moveit_E(self, dt, t):
+        self.t = 0
+        self.func = func
+        self.B = B
+
+    def __acceleration(self, v):
+        return (self.q/self.m)*(self.E+np.cross(v, self.B))
+
+    def __acceleration_f(self, v):
+        return (self.q/self.m)*(self.E+np.cross(v, self.func(self.t)))
+
+    def __move(self, dt):
+        k1v = self.__acceleration(self.v)*dt
+        k1r = self.v*dt 
+        k2v = self.__acceleration(self.v+k1v/2)*dt
+        k2r = (self.v+k2v/2)*dt
+        k3v = self.__acceleration(self.v+k2v/2)*dt
+        k3r = (self.v+k2v/2)*dt
+        k4v = self.__acceleration(self.v+k3v)*dt
+        k4r = (self.v+k3v)*dt
+        self.v += (k1v + 2*k2v + 2*k3v + k4v)/6
+        self.x.append(self.x[-1]+(k1r[0]+2*k2r[0]+2*k3r[0]+k4r[0])/6)
+        self.y.append(self.y[-1]+(k1r[1]+2*k2r[1]+2*k3r[1]+k4r[1])/6)
+        self.z.append(self.z[-1]+(k1r[2]+2*k2r[2]+2*k3r[2]+k4r[2])/6)   
+        self.t += dt     
+
+    def __move_f(self, dt):
+        k1v = self.__acceleration_f(self.v)*dt
+        k1r = self.v*dt 
+        k2v = self.__acceleration_f(self.v+k1v/2)*dt
+        k2r = (self.v+k2v/2)*dt
+        k3v = self.__acceleration_f(self.v+k2v/2)*dt
+        k3r = (self.v+k2v/2)*dt
+        k4v = self.__acceleration_f(self.v+k3v)*dt
+        k4r = (self.v+k3v)*dt
+        self.v += (k1v + 2*k2v + 2*k3v + k4v)/6
+        self.x.append(self.x[-1]+(k1r[0]+2*k2r[0]+2*k3r[0]+k4r[0])/6)
+        self.y.append(self.y[-1]+(k1r[1]+2*k2r[1]+2*k3r[1]+k4r[1])/6)
+        self.z.append(self.z[-1]+(k1r[2]+2*k2r[2]+2*k3r[2]+k4r[2])/6)   
+        self.t += dt 
+
+    def moveit_f(self, dt, t): 
         while self.t <= t:
-            self.B = self.function(self.t)
-            self.a = self.__acceleration(self.v, self.B)
-            self.v = np.add(self.v, self.a*dt)
-            self.r = np.add(self.r, self.v*dt)
-            self.t += dt
-            self.x.append(self.r[0])
-            self.y.append(self.r[1])
-            self.z.append(self.r[2])
+            self.__move_f(dt)
         return self.x, self.y, self.z
-    
-    def moveit_RK4(self, dt, t):
+
+    def moveit(self, dt, t): 
         while self.t <= t:
-            self.B = self.function(self.t)
-            k1v = self.__acceleration(self.v, self.B)*dt 
-            k1r = self.v*dt
-            k2v = self.__acceleration(np.add(self.v, k1v/2), self.B)*dt
-            k2r = (np.add(self.v,k1v/2))*dt
-            k3v = self.__acceleration(np.add(self.v, k2v/2), self.B)*dt
-            k3r = (np.add(self.v,k2v/2))*dt
-            k4v = self.__acceleration(np.add(self.v, k3v), self.B)*dt
-            k4r = (np.add(self.v,k3v))*dt
-            self.v = np.add(self.v,(1/6)*np.add(np.add(k1v,2*k2v),np.add(2*k3v,k4v)))
-            self.r = np.add(self.r,(1/6)*np.add(np.add(k1r,2*k2r),np.add(2*k3r,k4r)))
-            self.a = self.__acceleration(self.v, self.B)
-            self.x.append(self.r[0])
-            self.y.append(self.r[1])
-            self.z.append(self.r[2])
-            self.t += dt
-        return self.x, self.y, self.z
-    
+            self.__move(dt)
+        return self.x, self.y, self.z    
 
        
