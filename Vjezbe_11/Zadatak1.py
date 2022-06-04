cfeee2ae1e1553e82179bef2e9da022d96815e48
FabjanJozic <fjozic@pmfst.hr>
2022-06-04 15:15:32 +0200

    Zemljina orbita je elipticna te je period kraci od godine. To je tako zbog odsutnosti Mjeseca, koji usporava Zemlju i ispravlja joj orbitu svojim gravitacijskim utjecajem. Prikaz bi trebao biti tocno izracunat jer se koristi RK4.

--- Vjezbe_11/Zadatak1.py
+++ Vjezbe_11/Zadatak1.py
@@ -8,7 +8,7 @@ m_Earth = 5.9742*(10**24)
 m_Sun = 1.989*(10**30)
 r_Earth = np.array((1.496*(10**11), 0.))
 r_Sun = np.array((0., 0.))
-v_Earth = np.array((0., -29783.))
+v_Earth = np.array((0., 29783.))
 v_Sun = np.array((0., 0.))
 time = 60*60*24*365.242
 
