8fd32f8afb016853a67a7a1bf9fa8f2a1fb1e89e
FabjanJozic <fjozic@pmfst.hr>
2022-06-15 22:30:20 +0200

    Popravljanje vjezbi 11, sad je putanja kruznica.

--- Vjezbe_11/Zadatak1.py
+++ Vjezbe_11/Zadatak1.py
@@ -1,16 +1,39 @@
 import solar_system as ss
 import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-o = ss.System()
+au = 1.496*(10**11)
+dt = 10**4
+year = 60*60*24*365.242
+m = 5.9722*(10**24)         #mass of Earth
+M = 1.989*(10**30)          #mass of Sun
+r = 6.378*(10**6)           #radius of Earth
 
-m_Earth = 5.9742*(10**24)
-m_Sun = 1.989*(10**30)
-r_Earth = np.array((1.496*(10**11), 0.))
-r_Sun = np.array((0., 0.))
-v_Earth = np.array((0., 29783.))
-v_Sun = np.array((0., 0.))
-time = 60*60*24*365.242
+Sun = ss.Planet('Sun', M, 0., 0., 0., 109.178*r, 'yellow', 1000)
+Earth = ss.Planet('Earth', m, au, 0., 29783., r, 'blue', 100)
 
-o.set_initial_conditions(m_Earth, r_Earth, v_Earth, m_Sun, r_Sun, v_Sun)
-o.plot_revolution(time)
+system = ss.Universe()
+system.add_planet(Sun)
+system.add_planet(Earth)
+
+
+
+
+system.evolve(5*year, dt)
+
+fig = plt.figure(figsize=(12,6), dpi=110)
+plt.rcParams.update({'font.size': 8})                            #type: ignore
+axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+axes.axis('equal')
+axes.set_aspect('equal')
+axes.plot(Sun.x, Sun.y, color=Sun.collor, lw=0.4)
+axes.plot(Earth.x, Earth.y, color=Earth.collor, lw=0.4)
+axes.scatter(Sun.x[-1], Sun.y[-1], label=Sun.name, color=Sun.collor, s=Sun.size)
+axes.scatter(Earth.x[-1], Earth.y[-1], label=Earth.name, color=Earth.collor, s=Earth.size)
+axes.legend(loc='upper right')
+axes.set_facecolor("black")
+axes.grid(lw=0.1)
+axes.set_title('Solar system')
+plt.show()
--- Vjezbe_11/solar_system.py
+++ Vjezbe_11/solar_system.py
@@ -76,5 +76,60 @@ class System():
         axes.set_title('Sun and Earth')
         axes.legend(['Sun', 'Earth'], loc='best')
         return plt.show()
+    
+    
+    
+
+
+
+
+
+
+
+class Planet():
+    def __init__(self, name, mass, x0, y0, velocity, radius, collor, size):
+        self.velocity = np.array([0., velocity])
+        self.acceleration = np.array([0., 0.])
+        self.radius = radius
+        self.mass = mass
+        self.name = name
+        self.x = [x0]
+        self.y = [y0]
+        self.collor = collor
+        self.size = size
+
+
+
+class Universe():
+    def __init__(self):
+        self.planets = []
+        self.t = [0]
+        self.G = 6.67408*(10**(-11))
+        
+    def add_planet(self, planet):
+        self.planets.append(planet)
+        
+    def reset(self):
+        self.__init__()
+        
+    def __move(self, dt):
+        for planet1 in self.planets:
+            force = np.array([0., 0.])
+            for planet2 in self.planets:
+                if planet2 != planet1:
+                    r_x = planet1.x[-1]-planet2.x[-1]
+                    r_y = planet1.y[-1]-planet2.y[-1]
+                    r = np.sqrt(r_x**2+r_y**2)
+                    force[0] += -self.G*planet1.mass*planet2.mass/(r**2)*(r_x/r)
+                    force[1] += -self.G*planet1.mass*planet2.mass/(r**2)*(r_y/r)
+            planet1.acceleration = np.array([force[0]/planet1.mass, force[1]/planet1.mass])
+            planet1.velocity = np.array([planet1.velocity[0]+planet1.acceleration[0]*dt, planet1.velocity[1]+planet1.acceleration[1]*dt])
+            planet1.x.append(planet1.x[-1]+planet1.velocity[0]*dt)
+            planet1.y.append(planet1.y[-1]+planet1.velocity[1]*dt)
+        self.t.append(self.t[-1]+dt)       
+            
+    def evolve(self, time, dt):
+        while self.t[-1] <= time:
+            self.__move(dt)
         
             
