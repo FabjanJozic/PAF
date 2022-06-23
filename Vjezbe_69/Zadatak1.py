9d58af51341e67209df3efdf2d721f29300e6a36
FabjanJozic <fjozic@pmfst.hr>
2022-06-23 11:07:08 +0200

    popravljeni 1 i 2

--- Vjezbe_69/Zadatak1.py
+++ Vjezbe_69/Zadatak1.py
@@ -1,14 +1,12 @@
 class VertikalniHitac:
     def __init__(self):
         self.y = []
-        self.vx = []
-        self.vy = []
+        self.v = []
         
-    def set_initial_conditions(self, hight, horizontal_speed):
+    def set_initial_conditions(self, hight, speed):
         self.y.append(hight)
-        self.vx.append(horizontal_speed)
-        self.vy.append(0)
-        print('Objekt uspješno stvoren.\npočetna visina: {}'.format(self.y[-1]),'\npočetna horizontalna brzina: {}'.format(self.vx[-1]))
+        self.v.append(speed)
+        print('Objekt uspješno stvoren.\npočetna visina: {}'.format(self.y[-1]),'\npočetna brzina: {}'.format(self.v[-1]))
 
 
 
--- Vjezbe_69/Zadatak2.py
+++ Vjezbe_69/Zadatak2.py
@@ -1,19 +1,17 @@
 class VertikalniHitac:
     def __init__(self):
         self.y = []
-        self.vx = []
-        self.vy = []
+        self.v = []
         
-    def set_initial_conditions(self, hight, horizontal_speed):
+    def set_initial_conditions(self, hight, speed):
         self.y.append(hight)
-        self.vx.append(horizontal_speed)
-        self.vy.append(0)
+        self.v.append(speed)
         
     def change_hight(self, hight):
         self.y.append(hight)
         
     def change_velocity(self, speed):
-        self.vx.append(speed)
+        self.v.append(speed)
         
     def info(self):
-        print('\nvisina: {}'.format(self.y[-1]), '\nhorizontalna brzina: {}'.format(self.vx[-1]),'\n')
+        print('\nvisina: {}'.format(self.y[-1]), '\nbrzina: {}'.format(self.v[-1]),'\n')
