27d5aeaf9e8f1b0af1f42d64ae6e0c3edd952ce2
FabjanJozic <fjozic@pmfst.hr>
2022-06-23 10:39:42 +0200

    .

--- Vjezbe_69/Zadatak3.py
+++ Vjezbe_69/Zadatak3.py
@@ -12,6 +12,7 @@ class VertikalniHitac:
         self.ay = []
         self.t = []
         self.dt = []
+        self.v = []
         
     def set_initial_conditions(self, hight, horizontal_speed, dt):
         self.y.append(hight)
@@ -22,6 +23,7 @@ class VertikalniHitac:
         self.ay.append(-9.81)
         self.t.append(0)
         self.dt.append(dt)
+        self.v.append(np.sqrt((self.vx[-1])**2+(self.vy[-1])**2))
 
     def change_hight(self, hight):
         self.y.append(hight)
@@ -38,12 +40,14 @@ class VertikalniHitac:
         self.vy.append(self.vy[-1]+self.ay[-1]*dt)
         self.x.append(self.x[-1]+self.vx[-1]*dt)
         self.y.append(self.y[-1]+self.vy[-1]*dt)
-        return self.x, self.y, self.vx, self.vy, self.ax, self.ay, self.t
+        self.v.append(np.sqrt((self.vx[-1])**2+(self.vy[-1])**2))
+        return self.x, self.y, self.vx, self.vy, self.ax, self.ay, self.t, self.v
         
     def reset(self):
         self.__init__()
         
     def plot_h_t(self, hight, starting_speed, dt):
+        self.reset()
         self.set_initial_conditions(hight, starting_speed, dt)
         while self.y[-1] >= 0:
            self.__move(dt)
@@ -57,6 +61,21 @@ class VertikalniHitac:
         return plt.show()
     
     def plot_v_t(self, hight, starting_speed, dt):
+        self.reset()
+        self.set_initial_conditions(hight, starting_speed, dt)
+        while self.y[-1] >= 0:
+               self.__move(dt)
+        fig = plt.figure(figsize=(10,5), dpi=100)
+        axes = fig.add_axes([0.2, 0.2, 0.7, 0.7])
+        axes.plot(self.t, self.v, 'r')
+        axes.set_xlabel('t  $[m]$')
+        axes.set_ylabel('v  $[m/s]$')
+        axes.set_title('v-t graf gibanja')
+        axes.grid(lw=0.5)
+        return plt.show()
+    
+    def plot_vy_t(self, hight, starting_speed, dt):
+        self.reset()
         self.set_initial_conditions(hight, starting_speed, dt)
         while self.y[-1] >= 0:
                self.__move(dt)
@@ -82,3 +101,4 @@ dt = 0.01
 projectile = VertikalniHitac()
 projectile.plot_h_t(h, v, dt)
 projectile.plot_v_t(h, v, dt)
+projectile.plot_vy_t(h, v, dt)
