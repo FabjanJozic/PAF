adea6f14ba072e9a23fd9bd2a94b174068d5d835
FabjanJozic <fjozic@pmfst.hr>
2022-06-23 11:33:50 +0200

    Zadatak 5

--- Vjezbe_69/Zadatak5.py
+++ Vjezbe_69/Zadatak5.py
@@ -10,13 +10,17 @@ class VertikalniHitac:
         self.t = []
         self.dt = []
         self.a = []
+        self.g = []
+        self.k = []
         
-    def set_initial_conditions(self, hight, speed, dt):
+    def set_initial_conditions(self, hight, speed, constant, dt):
         self.y.append(hight)
-        self.a.append(-9.81)
+        self.a.append(0)
         self.t.append(0)
         self.dt.append(dt)
         self.v.append(speed)
+        self.k.append(constant)
+        self.g.append(9.81)
 
     def change_hight(self, hight):
         self.y.append(hight)
@@ -28,17 +32,17 @@ class VertikalniHitac:
         print('\nvisina: {}'.format(self.y[-1]), '\nbrzina: {}'.format(self.v[-1]),'\n')
         
     def __move(self, dt):
-        self.t.append(self.t[-1]+dt)
+        self.a.append(-self.g[-1]-np.sign(self.v[-1])*self.k[-1]*self.v[-1])
         self.v.append(self.v[-1]+self.a[-1]*dt)
         self.y.append(self.y[-1]+self.v[-1]*dt)
-        return  self.t, self.v, self.y
+        self.t.append(self.t[-1]+dt)
         
     def reset(self):
         self.__init__()
         
-    def plot_h_t(self, hight, starting_speed, dt):
+    def plot_h_t(self, hight, speed, constant, dt):
         self.reset()
-        self.set_initial_conditions(hight, starting_speed, dt)
+        self.set_initial_conditions(hight, speed, constant, dt)
         while self.y[-1] >= 0:
            self.__move(dt)
         fig = plt.figure(figsize=(10,5), dpi=100)
@@ -50,9 +54,9 @@ class VertikalniHitac:
         axes.grid(lw=0.5)
         return plt.show()
     
-    def plot_v_t(self, hight, starting_speed, dt):
+    def plot_v_t(self, hight, speed, constant, dt):
         self.reset()
-        self.set_initial_conditions(hight, starting_speed, dt)
+        self.set_initial_conditions(hight, speed, constant, dt)
         while self.y[-1] >= 0:
                self.__move(dt)
         fig = plt.figure(figsize=(10,5), dpi=100)
@@ -74,34 +78,25 @@ class VertikalniHitac:
             self.__move(dt)
         return max(self.t)
     
-
-
-
-
-
-
-
-
-projectile = VertikalniHitac()
-
-h = 30
-v = 170
-vremenski_korak = []
-vrijeme_pada = []
-dt = np.linspace(0.001, 0.1, 150)
-
-for i in range(len(dt)):
-    vremenski_korak.append(dt[i])
-    projectile.set_initial_conditions(h, v, dt[i])
-    vrijeme_pada.append(projectile.max_time(dt[i]))
-    projectile.reset()
     
-fig = plt.figure(figsize=(12,6), dpi=80)
-axes = fig.add_axes([0.15, 0.15, 0.75, 0.75])
-axes.plot(vremenski_korak, vrijeme_pada, 'k')
-axes.set_xlabel('dt  $[s]$')
-axes.set_ylabel('vrijeme pada  $[s]$')
-axes.set_title('Graf relativne pogreške vremena trajanja kosog hica u ovisnosti o odabranom koraku  dt')
-axes.xaxis.set_major_locator(tick.MultipleLocator(0.01))
-axes.grid(lw=0.5)
-plt.show()
+    
+
+
+h = 10
+v = 10
+k = 1.7
+dt1 = 0.01
+dt2 = 0.05
+dt3 = 0.1
+
+p1 = VertikalniHitac()
+p2 = VertikalniHitac()
+p3 = VertikalniHitac()
+
+p1.set_initial_conditions(h, v, k, dt1)
+p2.set_initial_conditions(h, v, k, dt2)
+p3.set_initial_conditions(h, v, k, dt3)
+
+print('Kada je dt={}, maksimalna visina je {}, a vrijeme pada je {}.\n'.format(dt1, p1.max_hight(dt1), p1.max_time(dt1)))
+print('Kada je dt={}, maksimalna visina je {}, a vrijeme pada je {}.\n'.format(dt2, p2.max_hight(dt2), p2.max_time(dt2)))
+print('Kada je dt={}, maksimalna visina je {}, a vrijeme pada je {}.\n'.format(dt3, p3.max_hight(dt3), p1.max_time(dt3)))
